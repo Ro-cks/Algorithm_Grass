b79e6b2b02b051540c0eb47ffc41b432e64d6313
Hyunwoo <66666401+Ro-cks@users.noreply.github.com>
2025-07-20 02:27:13 +0900

    [D2] Title: 숫자 배열 회전, Time: 74 ms, Memory: 25,856 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -2,76 +2,52 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-    
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static StringBuilder sb;
     public static StringTokenizer st;
-    
+
     public static void main(String[] args) throws IOException {
-                
         int T = Integer.parseInt(br.readLine());
-        
+
         for (int testCase = 1; testCase <= T; ++testCase) {
             int n = Integer.parseInt(br.readLine());
-            
             int[][] matrix = new int[n][n];
-            for (int i = 0; i < n; ++i) { // matrix init
+
+            for (int i = 0; i < n; ++i) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < n; ++j) {
                     matrix[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            
-            int[][] deg90Matrix = new int[n][n];
-            rotate(matrix, deg90Matrix, n);
-            
-            int[][] deg180Matrix = new int[n][n];
-            rotate(deg90Matrix, deg180Matrix, n);
-            
-            int[][] deg270Matrix = new int[n][n];
-            rotate(deg180Matrix, deg270Matrix, n);
-            
+
             sb = new StringBuilder();
-            
-            if (testCase != 1) {
-                sb.append('\n');
-            }
-            
-            sb.append('#').append(testCase);
-            
+            sb.append('#').append(testCase).append('\n');
+
             for (int i = 0; i < n; ++i) {
+                // 90도
+                for (int j = n - 1; j >= 0; --j) {
+                    sb.append(matrix[j][i]);
+                }
+                sb.append(' ');
+
+                // 180도
+                for (int j = n - 1; j >= 0; --j) {
+                    sb.append(matrix[n - 1 - i][j]);
+                }
+                sb.append(' ');
+
+                // 270도
+                for (int j = 0; j < n; ++j) {
+                    sb.append(matrix[j][n - 1 - i]);
+                }
                 sb.append('\n');
-                
-                for (int j = 0; j < n; ++j) {
-                    sb.append(deg90Matrix[i][j]);
-                }
-                
-                sb.append(' ');
-                
-                for (int j = 0; j < n; ++j) {
-                    sb.append(deg180Matrix[i][j]);
-                }
-                
-                sb.append(' ');
-                
-                for (int j = 0; j < n; ++j) {
-                    sb.append(deg270Matrix[i][j]);
-                }
             }
             
             bw.write(sb.toString());
         }
-        
+
         bw.flush();
         bw.close();
-    }
-    
-    public static void rotate(int[][] matrix, int[][] newMatrix, int length) {
-        for (int i = 0; i < length; ++i) { // matrix rotate
-            for (int j = length - 1; j >= 0; --j) {
-                newMatrix[j][length - 1 - i] = matrix[i][j];
-            }
-        }
     }
 }
