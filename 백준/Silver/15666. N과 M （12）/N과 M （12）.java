274971e122046e7b3d2edd910593eb954bd239cd
Hyunwoo <66666401+Ro-cks@users.noreply.github.com>
2025-08-11 15:03:20 +0900

    [Silver II] Title: N과 M (12), Time: 208 ms, Memory: 22216 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	static int N;
+	static int M;
+	static int[] nums;
+	static int[] sequence;
+	static boolean[] visited;
+	static LinkedHashSet<String> set = new LinkedHashSet<>();
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		nums = new int[N];
+		sequence = new int[M];
+		visited = new boolean[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; ++i) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(nums);
+		dfs(0, 0);
+		
+		set.forEach(System.out::println);
+	}
+	
+	static void dfs(int depth, int current) {
+		if (depth == M) {
+			sb = new StringBuilder();
+			for (int n : sequence) {
+				sb.append(n).append(' ');
+			}
+			
+			set.add(sb.toString());
+			
+			return;
+		}
+		
+		for (int i = current; i < N; ++i) {
+			sequence[depth] = nums[i];
+			dfs(depth + 1, i);
+		}
+	}
+}
