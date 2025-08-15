075e59b05942bcb156669f1ff7bc9c778f82d2da
Hyunwoo <66666401+Ro-cks@users.noreply.github.com>
2025-08-15 20:54:16 +0900

    [D4] Title: 정사각형 방, Time: 434 ms, Memory: 96,524 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -8,66 +8,73 @@ public class Solution {
 	
 	static int T;
 	static int N;
-	static int number;
-	static int length;
-	static int[][] grid;
-	static boolean[][] visited;
+	static int room;
+	static int answer;
+	static int[][] rooms;
 	
 	static int[][] dirs = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
 	
 	public static void main(String[] args) throws IOException {
-		T = Integer.parseInt(br.readLine());
+		T = Integer.parseInt(br.readLine().trim());
 		
 		for (int tc = 1; tc <= T; ++tc) {
 			init();
 			
 			for (int i = 0; i < N; ++i) {
 				for (int j = 0; j < N; ++j) {
-					visited[i][j] = true;
-					dfs(i, j, 1, grid[i][j]);
-					visited[i][j] = false;
+					solution(i, j);
 				}
 			}
 			
-			sb.append('#').append(tc).append(' ').append(number).append(' ').append(length).append('\n');
+			sb.append('#').append(tc).append(' ').append(room).append(' ').append(answer).append('\n');
 		}
 		
 		System.out.print(sb);
 	}
 	
-	static void dfs(int x, int y, int len, int startNumber) {
-		if (len > length) {
-			length = len;
-			number = startNumber;
-		} else if (len == length) {
-			number = Math.min(number, startNumber);
+	static void solution(int r, int c) {
+		Deque<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] {r, c});
+		int count = 1;
+		
+		while (!queue.isEmpty()) {
+			int[] cur = queue.poll();
+			int cr = cur[0];
+			int cc = cur[1];
+			
+			for (int[] dir : dirs) {
+				int nr = cr + dir[0];
+				int nc = cc + dir[1];
+				
+				if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+				
+				if (rooms[nr][nc] == rooms[cr][cc] + 1) {
+					++count;
+					queue.offer(new int[] {nr, nc});
+					
+					continue;
+				}
+			}
 		}
 		
-		for (int[] dir : dirs) {
-			int nx = x + dir[0];
-			int ny = y + dir[1];
-			
-			if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-			if (visited[nx][ny]) continue;
-			
-			if (grid[nx][ny] == grid[x][y] + 1) {
-				visited[nx][ny] = true;
-				dfs(nx, ny, len + 1, startNumber);
-				visited[nx][ny] = false;
-			}
+		if (count > answer) {
+			answer = count;
+			room = rooms[r][c];
+		} else if (count == answer) {
+			room = Math.min(room, rooms[r][c]);
 		}
 	}
 	
 	static void init() throws IOException {
-		N = Integer.parseInt(br.readLine());
-		length = 0;
-		visited = new boolean[N][N];
+		N = Integer.parseInt(br.readLine().trim());
+		room = (int) Math.pow(N, 2);
+		answer = 0;
 		
-		grid = new int[N][N];
+		rooms = new int[N][N];
 		for (int i = 0; i < N; ++i) {
-			st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine().trim());
 			for (int j = 0; j < N; ++j) {
-				grid[i][j] = Integer.parseInt(st.nextToken());
+				rooms[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 	}
