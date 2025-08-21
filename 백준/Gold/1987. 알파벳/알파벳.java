57e5fa9cc9945e9aa4e6043fa385e116bcfc27a4
Hyunwoo <66666401+Ro-cks@users.noreply.github.com>
2025-08-21 21:51:28 +0900

    [Gold IV] Title: 알파벳, Time: 712 ms, Memory: 11840 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -2,67 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	
-	static int R;
-	static int C;
-	static int answer;
-	static char[][] board;
-	static boolean[] visited = new boolean[26];
-	
-	static final int[] dr = {-1, 1, 0, 0};
+    static int R, C, ans = 1;
+    static char[][] board;
+    static final int[] dr = {-1, 1, 0, 0};
     static final int[] dc = {0, 0, -1, 1};
-	
-	public static void main(String[] args) throws IOException {
-		init();
-		
-		solution();
-		
-		System.out.print(answer);
-	}
-	
-	static void solution() {
-		visited[board[0][0] - 'A'] = true;
-		DFS(0, 0, 1);
-	}
-	
-	static void DFS(int r, int c, int count) {
-		answer = Math.max(answer, count);
-		
-		for (int d = 0; d < 4; ++d) {
-			int nr = r + dr[d];
-			int nc = c + dc[d];
-			
-			if (nr < 0 || nr >= R || nc < 0 || nc >= C) {
-				
-				continue;
-			}
-			
-			if (visited[board[nr][nc] - 'A']) {
-				
-				continue;
-			}
 
-			visited[board[nr][nc] - 'A'] = true;
-			DFS(nr, nc, count + 1);
-			visited[board[nr][nc] - 'A'] = false;
-		}
-	}
-	
-	static void init() throws IOException {
-		st = new StringTokenizer(br.readLine().trim());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		answer = Integer.MIN_VALUE;
-		
-		board = new char[R][C];
-		for (int r = 0; r < R; ++r) {
-			String input = br.readLine();
-			
-			for (int c = 0; c < C; ++c) {
-				board[r][c] = input.charAt(c);
-			}
-		}
-	}
+    static void dfs(int r, int c, int mask, int depth) {
+        ans = Math.max(ans, depth);
+
+        for (int k = 0; k < 4; k++) {
+            int nr = r + dr[k], nc = c + dc[k];
+            if (nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
+
+            int bit = 1 << (board[nr][nc] - 'A');
+            if ((mask & bit) != 0) continue;        // 이미 사용한 알파벳
+
+            dfs(nr, nc, mask | bit, depth + 1);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        board = new char[R][C];
+        for (int i = 0; i < R; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < C; j++) board[i][j] = s.charAt(j);
+        }
+
+        int startMask = 1 << (board[0][0] - 'A');
+        dfs(0, 0, startMask, 1);
+        System.out.print(ans);
+    }
 }
