04d7b1167a4993b1abc385622d104f2a7d5d9eff
Hyunwoo <66666401+Ro-cks@users.noreply.github.com>
2025-08-30 19:00:51 +0900

    [Silver IV] Title: 상근이의 여행, Time: 232 ms, Memory: 31284 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	static int T;
+	static int N;
+	static int M;
+	
+	public static void main(String[] args) throws IOException {
+		T = Integer.parseInt(br.readLine().trim());
+		
+		for (int tc = 1; tc <= T; ++tc) {
+			init();
+			
+			sb.append(N - 1).append('\n');
+		}
+		
+		System.out.print(sb);
+	}
+	
+	static void init() throws IOException {
+		st = new StringTokenizer(br.readLine().trim());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		for (int i = 0; i < M; ++i) {
+			st = new StringTokenizer(br.readLine().trim());
+		}
+	}
+}
