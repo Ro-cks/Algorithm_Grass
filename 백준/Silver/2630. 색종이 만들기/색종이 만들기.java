8794d04bb9c411cf8505f31c8001a31c7cd1a617
Hyunwoo <66666401+Ro-cks@users.noreply.github.com>
2025-09-01 01:46:18 +0900

    [Silver II] Title: 색종이 만들기, Time: 148 ms, Memory: 15768 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -3,60 +3,67 @@ import java.util.*;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st;
 	
 	static int N;
-	static int[] counts;
-	static int[][] nums;
+	static int[] answer;
+	static int[][] map;
 	
 	public static void main(String[] args) throws IOException {
 		init();
 		
 		cut(0, 0, N);
 		
-		System.out.print(counts[0] + "\n" + counts[1]);
+		sb.append(answer[0]).append('\n');
+		sb.append(answer[1]);
+		
+		System.out.print(sb);
 	}
 	
 	static void cut(int r, int c, int size) {
-		int num = nums[r][c];
+		int num = map[r][c];
+		
 		if (size == 1) {
-			++counts[num];
+			++answer[num];
 			
 			return;
 		}
 		
 		boolean isSame = true;
-		
 		for (int i = r; i < r + size; ++i) {
 			for (int j = c; j < c + size; ++j) {
-				if (num != nums[i][j]) {
+				if (map[i][j] != num) {
 					isSame = false;
 				}
 			}
 		}
 		
 		if (isSame) {
-			++counts[num];
+			++answer[num];
 		} else {
 			int newSize = size / 2;
-			
+			// topLeft
 			cut(r, c, newSize);
-			cut(r + newSize, c, newSize);
+			// topRight
 			cut(r, c + newSize, newSize);
+			// bottomLeft
+			cut(r + newSize, c, newSize);
+			// bottomRight
 			cut(r + newSize, c + newSize, newSize);
 		}
 	}
 	
 	static void init() throws IOException {
 		N = Integer.parseInt(br.readLine().trim());
-		counts = new int[2];
-		nums = new int[N][N];
+		answer = new int[2];
+		map = new int[N][N];
 		
 		for (int i = 0; i < N; ++i) {
 			st = new StringTokenizer(br.readLine().trim());
 			
 			for (int j = 0; j < N; ++j) {
-				nums[i][j] = Integer.parseInt(st.nextToken());
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 	}
