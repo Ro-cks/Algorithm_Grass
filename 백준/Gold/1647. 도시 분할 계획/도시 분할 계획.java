6de0cf5aef2b8b6ac76773aa10122a553d5239a8
Hyunwoo <66666401+Ro-cks@users.noreply.github.com>
2025-09-01 00:14:49 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1552 ms, Memory: 327740 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,105 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	
+	static int N;
+	static int M;
+	static int answer;
+	static int[] sets;
+	static List<Edge> edges;
+	
+	private static class Edge implements Comparable<Edge> {
+		int u;
+		int v;
+		int cost;
+		
+		public Edge(int u, int v, int cost) {
+			this.u = u;
+			this.v = v;
+			this.cost = cost;
+		}
+		
+		@Override
+		public int compareTo(Edge e) {
+			
+			return Integer.compare(this.cost, e.cost);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		init();
+		
+		solution();
+		
+		System.out.print(answer);
+	}
+	
+	static void solution() {
+		int count = 0;
+		
+		for (Edge edge : edges) {
+			if (union(edge.u, edge.v)) {
+				++count;
+				answer += edge.cost;
+				
+				if (count == N - 1) {
+					answer -= edge.cost;
+					
+					return;
+				}
+			}
+		}
+	}
+	
+	static boolean union(int a, int b) {
+		int pa = find(a);
+		int pb = find(b);
+		
+		if (pa == pb) {
+			
+			return false;
+		} else if (pa < pb) {
+			sets[pb] = pa;
+		} else {
+			sets[pa] = pb;
+		}
+		
+		return true;
+	}
+	
+	static int find(int a) {
+		if (sets[a] == a) {
+			
+			return a;
+		}
+		
+		return sets[a] = find(sets[a]);
+	}
+	
+	static void init() throws IOException {
+		st = new StringTokenizer(br.readLine().trim());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		answer = 0;
+		
+		sets = new int[N + 1];
+		for (int i = 0; i <= N; ++i) {
+			sets[i] = i;
+		}
+		
+		edges = new ArrayList<>();
+		for (int i = 0; i < M; ++i) {
+			st = new StringTokenizer(br.readLine().trim());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			
+			edges.add(new Edge(u, v, cost));
+		}
+		
+		Collections.sort(edges);
+	}
+}
