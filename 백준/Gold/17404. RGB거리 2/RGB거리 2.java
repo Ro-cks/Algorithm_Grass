79cd646c9fee435513c6e0cf0e0b9b09a1ab8582
Hyunwoo <66666401+Ro-cks@users.noreply.github.com>
2025-09-14 21:15:13 +0900

    [Gold IV] Title: RGB거리 2, Time: 112 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -42,41 +42,19 @@ public class Main {
 	static void solution() throws IOException {
 		int answer = Integer.MAX_VALUE;
 		
-		dp[0][0] = costs[0][0];
-		dp[0][1] = 1001 * 1000;
-		dp[0][2] = 1001 * 1000;
-		
-		for (int i = 1; i < N; ++i) {
-			dp[i][0] = costs[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
-	        dp[i][1] = costs[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
-	        dp[i][2] = costs[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+		for (int i = 0; i < 3; ++i) {
+			dp[0][i] = costs[0][i];
+			dp[0][(i + 1) % 3] = 1001 * 1000;
+			dp[0][(i + 2) % 3] = 1001 * 1000;
+			
+			for (int j = 1; j < N; ++j) {
+				dp[j][0] = costs[j][0] + Math.min(dp[j - 1][1], dp[j - 1][2]);
+		        dp[j][1] = costs[j][1] + Math.min(dp[j - 1][0], dp[j - 1][2]);
+		        dp[j][2] = costs[j][2] + Math.min(dp[j - 1][0], dp[j - 1][1]);
+			}
+			
+			answer = Math.min(answer, Math.min(dp[N - 1][(i + 1) % 3], dp[N - 1][(i + 2) % 3]));
 		}
-		
-		answer = Math.min(answer, Math.min(dp[N - 1][1], dp[N - 1][2]));
-		
-		dp[0][0] = 1001 * 1000;
-	    dp[0][1] = costs[0][1];
-	    dp[0][2] = 1001 * 1000;
-	    
-	    for (int i = 1; i < N; ++i) {
-	        dp[i][0] = costs[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
-	        dp[i][1] = costs[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
-	        dp[i][2] = costs[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
-	    }
-	    
-	    answer = Math.min(answer, Math.min(dp[N - 1][0], dp[N - 1][2]));
-	    
-	    dp[0][0] = 1001 * 1000;
-	    dp[0][1] = 1001 * 1000;
-	    dp[0][2] = costs[0][2];
-	    
-	    for (int i = 1; i < N; ++i) {
-	        dp[i][0] = costs[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
-	        dp[i][1] = costs[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
-	        dp[i][2] = costs[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
-	    }
-	    
-	    answer = Math.min(answer, Math.min(dp[N - 1][0], dp[N - 1][1]));
 
 	    System.out.print(answer);
 	}
