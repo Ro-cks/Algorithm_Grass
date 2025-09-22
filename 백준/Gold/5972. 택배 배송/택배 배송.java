508630add85e5884e7d646b2cea7fb88187dcf8c
Hyunwoo <66666401+Ro-cks@users.noreply.github.com>
2025-09-23 00:12:59 +0900

    [Gold V] Title: 택배 배송, Time: 384 ms, Memory: 40940 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -7,11 +7,12 @@ public class Main {
 	
 	static int N;
 	static int M;
-	static int answer;
-	static int[] costs;
+	static int[] D;
 	static List<Node>[] graph;
 	
-	private static class Node {
+	static final int INF = 50000000;
+	
+	static class Node {
 		int num;
 		int cost;
 		
@@ -25,25 +26,6 @@ public class Main {
 		init();
 		
 		solution();
-		
-		System.out.print(costs[N]);
-	}
-	
-	static void solution() {
-		PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
-		pq.offer(new Node(1, 0));
-		
-		while (!pq.isEmpty()) {
-			Node curr = pq.poll();
-			
-			for (Node next : graph[curr.num]) {
-				if (costs[next.num] > costs[curr.num] + next.cost) {
-					costs[next.num] = costs[curr.num] + next.cost;
-					
-					pq.offer(next);
-				}
-			}
-		}
 	}
 	
 	static void init() throws IOException {
@@ -51,24 +33,51 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		costs = new int[N + 1];
-		Arrays.fill(costs, Integer.MAX_VALUE);
-		costs[1] = 0;
-		
+		D = new int[N + 1];
 		graph = new ArrayList[N + 1];
 		for (int i = 1; i <= N; ++i) {
+			D[i] = INF;
 			graph[i] = new ArrayList<>();
 		}
 		
+		D[1] = 0;
+		
 		for (int i = 0; i < M; ++i) {
 			st = new StringTokenizer(br.readLine().trim());
-			
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			int cost = Integer.parseInt(st.nextToken());
 			
 			graph[u].add(new Node(v, cost));
 			graph[v].add(new Node(u, cost));
+		}
+	}
+	
+	static void solution() {
+		dijkstra();
+		
+		System.out.print(D[N]);
+	}
+	
+	static void dijkstra() {
+		PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
+		boolean[] visited = new boolean[N + 1];
+		pq.offer(new Node(1, 0));
+		
+		while (!pq.isEmpty()) {
+			Node curr = pq.poll();
+			
+			if (visited[curr.num]) continue;
+			visited[curr.num] = true;
+			
+			for (Node next : graph[curr.num]) {
+				if (visited[next.num]) continue;
+				
+				if (D[next.num] > D[curr.num] + next.cost) {
+					D[next.num] = D[curr.num] + next.cost;
+					pq.offer(new Node(next.num, D[next.num]));
+				}
+			}
 		}
 	}
 }
