b470b67a6865713f307a935a06ef794848051cbc
Hyunwoo <66666401+Ro-cks@users.noreply.github.com>
2025-11-17 02:03:17 +0900

    [Gold V] Title: 집합의 표현, Time: 392 ms, Memory: 47216 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -6,8 +6,8 @@ public class Main {
 	static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st;
 	
-	static int N;
-	static int M;
+	static int n;
+	static int m;
 	static int[] sets;
 	
 	static final String YES = "yes";
@@ -19,53 +19,56 @@ public class Main {
 		solution();
 	}
 	
+	static void init() throws IOException {
+		st = new StringTokenizer(br.readLine().trim());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		sets = new int[n + 1];
+		for (int i = 1; i <= n; ++i) {
+			sets[i] = i;
+		}
+	}
+	
 	static void solution() throws IOException {
-		for (int i = 0; i < M; ++i) {
+		for (int i = 0; i < m; ++i) {
 			st = new StringTokenizer(br.readLine().trim());
 			
 			int cmd = Integer.parseInt(st.nextToken());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			
-			switch (cmd) {
-				case 0:
-					union(a, b);
-					break;
-				default:
-					if (find(a) == find(b)) sb.append(YES).append('\n');
-					else sb.append(NO).append('\n');
+			if (cmd == 0) {
+				union(a, b);
+			} else {
+				if (find(a) == find(b)) sb.append(YES).append('\n');
+				else sb.append(NO).append('\n');
 			}
 		}
 		
 		System.out.print(sb);
 	}
 	
-	static int find(int start) {
-		if (sets[start] == start) {
+	static int find(int a) {
+		if (sets[a] == a) {
 			
-			return start;
+			return a;
 		}
 		
-		return sets[start] = find(sets[start]);
+		return sets[a] = find(sets[a]);
 	}
 	
-	static void union(int a, int b) {
-		int setA = find(a);
-		int setB = find(b);
+	static boolean union(int a, int b) {
+		int pa = find(a);
+		int pb = find(b);
 		
-		int repreSet = setA <= setB ? setA : setB;
+		if (pa == pb) {
+			
+			return false;
+		} 
+
+		sets[pb] = pa;
 		
-		sets[setB] = setA;
-	}
-	
-	static void init() throws IOException {
-		st = new StringTokenizer(br.readLine().trim());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		sets = new int[N + 1];
-		for (int i = 0; i <= N; ++i) {
-			sets[i] = i;
-		}
+		return true;
 	}
 }
