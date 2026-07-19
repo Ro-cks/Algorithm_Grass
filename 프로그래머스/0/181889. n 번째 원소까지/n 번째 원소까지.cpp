1b0109e8e77da29bc7ab72a4867e15829a28c9cf
Hyunwoo <66666401+Ro-cks@users.noreply.github.com>
2026-07-19 21:25:55 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 4.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer(n);
+    
+    for (int i = 0; i < n; ++i) {
+        answer[i] = num_list[i];
+    }
+    
+    return answer;
+}
